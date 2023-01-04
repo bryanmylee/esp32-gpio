e56b8653d29fcdba7526981b35181274c485fcc7
Bryan Lee <bryanmylee@gmail.com>
2023-01-04 20:38:46 +0800

    wifi server

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,110 @@
 #include <Arduino.h>
-#include "WiFi.h"
+#include <WiFi.h>
+
+#define LED_PIN 4
+
+const char *SSID = "XXXX";
+const char *PASS = "XXXX";
+
+// Initialize the WiFi server.
+WiFiServer server(80);
 
 void setup()
 {
   Serial.begin(9600);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
+  pinMode(LED_PIN, OUTPUT);
 
-  Serial.println("Setup done");
+  delay(10);
+
+  Serial.printf("Connecting to %s\n", SSID);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASS);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
 }
+
+int value = 0;
 
 void loop()
 {
-  Serial.println("Start scan");
+  // Listen for incoming clients.
+  WiFiClient client = server.available();
 
-  // WiFi.scanNetworks will return the number of networks found.
-  int networkCount = WiFi.scanNetworks();
-  Serial.println("Scan complete");
-  if (networkCount == 0)
+  // If a client is received.
+  if (client)
   {
-    Serial.println("No networks found");
-  }
-  else
-  {
-    for (int i = 0; i < networkCount; ++i)
+    Serial.println("New client.");
+    // Allocate a String to hold incoming data from the client.
+    String currentLine = "";
+    // Blocking listen to the connected client.
+    while (client.connected())
     {
-      Serial.printf("%d: %s (%d) %s\n",
-                    i + 1,
-                    WiFi.SSID(i),
-                    WiFi.RSSI(i),
-                    WiFi.encryptionType(i) == WIFI_AUTH_OPEN ? " " : "*");
-      delay(10);
-    }
-  }
-  Serial.println("");
+      // Check if incoming bytes are available from the client.
+      if (client.available())
+      {
+        // Read a single byte.
+        char input = client.read();
+        Serial.write(input);
+        if (input == '\n')
+        {
+          // If the current line is blank, then we've received two newline
+          // characters in a row. That's the end of the HTTP request, so send a
+          // response.
+          if (currentLine.length() == 0)
+          {
+            /// HEADER
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
 
-  delay(5000);
+            /// CONTENT
+            // The content of the HTTP response follows the header.
+            client.print("<a href=\"/H\">Turn the LED on</a><br>");
+            client.print("<a href=\"/L\">Turn the LED off</a><br>");
+
+            /// END
+            client.println();
+            break;
+          }
+          else
+          {
+            // Otherwise, clear the currentLine if a newline is received.
+            currentLine = "";
+          }
+        }
+        else if (input != '\r')
+        {
+          // Anything else but a carriage return should be added to the end of
+          // the currentLine.
+          currentLine += input;
+        }
+
+        if (currentLine.endsWith("GET /H"))
+        {
+          digitalWrite(LED_PIN, HIGH);
+        }
+        else if (currentLine.endsWith("GET /L"))
+        {
+          digitalWrite(LED_PIN, LOW);
+        }
+      }
+    }
+    // Once the client is no longer connected, close the connection.
+    client.stop();
+    Serial.println("Client disconnected.");
+  }
 }
